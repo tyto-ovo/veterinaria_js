d23ca47e917a5360eef3f5787f02d83505bc142b
tyto-ovo <sjollyparker@gmail.com>
2025-04-21 12:22:17 -0400

    funcionamiento index

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const fs = require("fs");
+const { registrar, leer } = require("./operaciones");
+
+const argumento = process.argv.slice(2);
+
+console.log(argumento);
+
+if (argumento[0] === "registrar") {
+  registrar(
+    argumento[1],
+    argumento[2],
+    argumento[3],
+    argumento[4],
+    argumento[5]
+  );
+} else if (argumento[0] === "leer") {
+  leer();
+} else {
+  console.log("ingrese una funcion valida");
+}
